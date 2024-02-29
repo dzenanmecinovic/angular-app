79834ae17e68f294c771a9fef86f249126652ac5
Dženan Mecinović <mecinovicdz@gmail.com>
2024-02-29 15:47:31 +0100

    component selectors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css'],
-  styles: [
-    `
-      h3 {
-        color: dodgerblue;
-        font-weight: bold;
-      }
-    `,
-  ], // inline styles is taking over
+  styleUrls: ['./app.component.css'],
+  // styles: [
+  //   `
+  //     h3 {
+  //       color: dodgerblue;
+  //       font-weight: bold;
+  //     }
+  //   `,
+  // ], // inline styles is taking over
 })
 export class AppComponent {}

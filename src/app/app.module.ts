d89987be2682a031915631ed65d48ec6430f6e86
Dženan Mecinović <mecinovicdz@gmail.com>
2024-02-29 02:52:03 +0100

    fresh start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import {
   BrowserModule,
   provideClientHydration,
 } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
 })
